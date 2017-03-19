b5bb283795803fcd9e1c66a7f65aea8833770ade
Ashhar Hasan <hashhar_dev@outlook.com>
2017-03-19 10:40:48 +0530

    Add pacaur variables

--- bash/.config/bash/variables.sh
+++ bash/.config/bash/variables.sh
@@ -1,7 +1,13 @@
+# Pacaur {{{
+export AURDEST='/home/ashhar/makepkg/sources/'
+# }}}
+
+# Browser {{{
 if [[ -n "$DISPLAY" ]]; then
 	export BROWSER=firefox
 else
 	export BROWSER=elinks
 fi
+# }}}
 
 # vim: tw=80 fdm=marker
