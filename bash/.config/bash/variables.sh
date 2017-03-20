d16c1d2a218ba4bffc87911095dc37ce83c73009
Ashhar Hasan <hashhar_dev@outlook.com>
2017-03-20 23:39:01 +0530

    Move some vars from PAM and start cava config

--- bash/.config/bash/variables.sh
+++ bash/.config/bash/variables.sh
@@ -1,6 +1,6 @@
-# Pacaur {{{
-export AURDEST='/home/ashhar/makepkg/sources/'
-# }}}
+# Default programs {{{
+export EDITOR=vim
+export VISUAL=vim
 
 # Browser {{{
 if [[ -n "$DISPLAY" ]]; then
@@ -8,6 +8,18 @@ if [[ -n "$DISPLAY" ]]; then
 else
 	export BROWSER=elinks
 fi
+# }}}
+# }}}
+
+# Mount point variables {{{
+Resources	DEFAULT=/media/Resources
+Media		DEFAULT=/media/Media
+Data		DEFAULT=/media/Data
+GitHub		DEFAULT=${Resources}/GitHub
+# }}}
+
+# Pacaur {{{
+export AURDEST='/home/ashhar/makepkg/sources/'
 # }}}
 
 # vim: tw=80 fdm=marker
