1626759e07b8b8c23e02010704a34015b90e13be
Ashhar Hasan <hashhar_dev@outlook.com>
2017-05-02 02:13:53 +0530

    Fix: sl didn't work due to cascade of aliases

--- bash/.config/bash/aliases.sh
+++ bash/.config/bash/aliases.sh
@@ -1,7 +1,7 @@
 # ls {{{
+alias sl='ls --group-directories-first --color=auto -h'
 alias ls='ls --group-directories-first --color=auto -h -A'
 alias ll='ls --group-directories-first --color=auto -hF -lA'
-alias sl='ls --group-directories-first --color=auto -h'
 alias tree='tree -C'
 # }}}
 
