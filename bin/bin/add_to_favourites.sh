627ec686b86d5c4d55a1478cb4ad0e778f33bbf8
Ashhar Hasan <hashhar_dev@outlook.com>
2017-08-28 16:13:08 +0530

    Add: A script to maintain a Favourites playlist for mpd

---
+++ bin/bin/add_to_favourites.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+file="$(mpc current -f %file%)"
+current="$(mpc current -f "[[%albumartist%|%artist% - ]%title%]|[%file%]")"
+
+playlist="$HOME/.config/mpd/playlists/Favourites.m3u"
+if [ "$file" != "" ]; then
+	echo "$file" >> "$playlist"
+	sort -u "$playlist" -o "$playlist"
+	notify-send -t 3000 "MPD" "Successfully added <b>${current//&/&amp;}</b> to Favourites"
+	exit 0
+else
+	notify-send -t 3000 "MPD" "Nothing playing right now"
+	exit 1
+fi
