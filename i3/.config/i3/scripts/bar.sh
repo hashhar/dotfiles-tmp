911ffb59c0d9766e0fe5d1cab334b28550288bbe
Ashhar Hasan <hashhar_dev@outlook.com>
2017-08-30 12:35:43 +0530

    Update: mpd status bar script to be much better

--- i3/.config/i3/scripts/bar.sh
+++ i3/.config/i3/scripts/bar.sh
@@ -4,21 +4,29 @@ i3status | while :
 do
     read line
 
-    printf -v mpd_song "%q" "$(mpc current)"
-    status="$(mpc status)"
-    pos="$(echo "$status" | grep -o -P "\\d+:\\d+/\\d+:\\d+")"
-    pos="${pos//\// of }"
+    printf -v mpd_song "%q" "$(mpc current -f "[[%albumartist%|%artist% - ]%title%]")"
+    # printf -v sets the variable to '' in case of empty assignment
+    if [[ "$mpd_song" == "''" ]]; then
+        printf -v mpd_song "%q" "$(basename "$(mpc current -f "%file%")")"
+    fi
 
-    # printf -v sets the variable to a pair of single quotes if the assignment is empty
-    if [ "$mpd_song" != "''" ]; then
-        mpd_status="$(echo "$status" | tail -2 | head -1 | cut -d' ' -f1 | tr -d '[]')"
-        if [ "$mpd_status" = "playing" ]; then
-            color="#00AAAA"
-        else
-            color="#AAAA00"
-        fi
-        echo "$(echo "$line" | sed 's|{\"name|{\"name\":\"music\",\"color\": \"'"$color"'\",\"full_text\":\"'"$mpd_song"' - '"$pos"'\"},{\"name|')" || exit 1
+    status="$(mpc status)"
+    mpd_status="$(echo "$status" | tail -2 | head -1 | cut -d' ' -f1 | tr -d '[]')"
+    flag=1
+
+    case "$mpd_status" in
+        "playing" )
+            color="#00AAAA" ;;
+        "paused" )
+            color="#AAAA00" ;;
+        * )
+            flag=0 ;;
+    esac
+    if (( flag == 1 )); then
+        echo "$line" | sed 's|{\"name|{\"name\":\"music\",\"color\": \"'"$color"'\",\"full_text\":\"  '"$mpd_song"'\"},{\"name|' || exit 1
+        continue
+    else
+        echo -n "$line" || exit 1
         continue
     fi
-    echo -n "$line" || exit 1
 done
