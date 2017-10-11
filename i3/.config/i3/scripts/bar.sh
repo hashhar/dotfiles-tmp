d87b0f9926ae26ef0509343ccf9606b7013e1cff
Ashhar Hasan <hashhar_dev@outlook.com>
2017-10-11 17:08:49 +0530

    Remove: mpd repeat, single, random, consume status

--- i3/.config/i3/scripts/bar.sh
+++ i3/.config/i3/scripts/bar.sh
@@ -4,13 +4,6 @@ i3status | while :
 do
     read -r line
 
-    # Get repeat, random, single and consume mode from mpd
-    output=( $(mpc status | tail -1 | grep -o ": [[:alpha:]]\+" | tr -d ': ') )
-    repeat=${output[0]}
-    random=${output[1]}
-    single=${output[2]}
-    consume=${output[3]}
-
     printf -v mpd_song "%q" "$(mpc current -f "[[%albumartist%|%artist% - ]%title%]")"
     # printf -v sets the variable to '' in case of empty assignment
     if [[ "$mpd_song" == "''" ]]; then
@@ -30,7 +23,7 @@ do
             flag=0 ;;
     esac
     if (( flag == 1 )); then
-        echo "$line" | sed 's|{\"name|{\"name\":\"mpdstats\",\"full_text\":\"'"r: $repeat z: $random s: $single c: $consume"'\"},{\"name\":\"music\",\"color\": \"'"$color"'\",\"full_text\":\"  '"$mpd_song"'\"},{\"name|' || exit 1
+        echo "$line" | sed 's|{\"name|{\"name\":\"music\",\"color\": \"'"$color"'\",\"full_text\":\"  '"$mpd_song"'\"},{\"name|' || exit 1
         continue
     else
         echo -n "$line" || exit 1
