205af14fa4155166af58c3130fcbf2e390a549be
Ashhar Hasan <hashhar_dev@outlook.com>
2017-10-12 14:52:47 +0530

    Add: Random wallpapers, maintain two copies of favourites playlist

--- bin/bin/add_to_favourites.sh
+++ bin/bin/add_to_favourites.sh
@@ -3,10 +3,11 @@
 file="$(mpc current -f %file%)"
 current="$(mpc current -f "[[%albumartist%|%artist% - ]%title%]|[%file%]")"
 
-playlist="$HOME/.config/mpd/playlists/Favourites.m3u"
+playlist="$HOME/.config/mpd/playlists/Favourites.m3u.bup"
+fixedplaylist="$HOME/.config/mpd/playlists/Favourites.m3u"
 if [ "$file" != "" ]; then
 	echo "$file" >> "$playlist"
-	sort -u "$playlist" -o "$playlist"
+	sort -u "$playlist" -o "$fixedplaylist"
 	notify-send -t 3000 "MPD" "Successfully added <b>${current//&/&amp;}</b> to Favourites"
 	exit 0
 else
---
+++ bin/bin/random_wall.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+DIR="$HOME/Pictures/Wallpapers"
+
+if [ ! -d "$DIR" ]; then
+    echo "Not Exist $DIR"
+    exit 1
+fi
+
+setbg () {
+    NUM_FILES=$( find "$DIR" -type f | wc -l )
+    RAND_NUM=$(( RANDOM % NUM_FILES ))
+    test ! $RAND_NUM = 0 || RAND_NUM=1
+
+    RAND_FILE="$( find "$DIR" -type f | head -n $RAND_NUM | tail -n 1)"
+
+    feh --bg-max "${RAND_FILE}"
+}
+
+if [ -f "/tmp/random_wall.lock" ]; then
+    setbg
+    exit 1
+fi
+
+touch /tmp/random_wall.lock
+
+while true; do
+    setbg
+    sleep 3600
+    rm /tmp/random_wall.lock
+    touch /tmp/random_wall.lock
+done
