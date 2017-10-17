e7d52fb2c7b3f0eed1b89b1c33704a8c420c9879
Ashhar Hasan <hashhar_dev@outlook.com>
2017-10-17 15:14:01 +0530

    Fix: Remove temp playlist to avoid issues when removing favourites

--- bin/bin/add_to_favourites.sh
+++ bin/bin/add_to_favourites.sh
@@ -3,11 +3,10 @@
 file="$(mpc current -f %file%)"
 current="$(mpc current -f "[[%albumartist%|%artist% - ]%title%]|[%file%]")"
 
-playlist="$HOME/.config/mpd/playlists/Favourites.m3u.bup"
-fixedplaylist="$HOME/.config/mpd/playlists/Favourites.m3u"
+playlist="$HOME/.config/mpd/playlists/Favourites.m3u"
 if [ "$file" != "" ]; then
 	echo "$file" >> "$playlist"
-	sort -u "$playlist" -o "$fixedplaylist"
+	sort -u "$playlist" -o "$playlist"
 	notify-send -t 3000 "MPD" "Successfully added <b>${current//&/&amp;}</b> to Favourites"
 	exit 0
 else
--- bin/bin/remove_from_favourites.sh
+++ bin/bin/remove_from_favourites.sh
@@ -3,14 +3,13 @@
 file="$(mpc current -f %file%)"
 current="$(mpc current -f "[[%albumartist%|%artist% - ]%title%]|[%file%]")"
 
-playlist="$HOME/.config/mpd/playlists/Favourites.m3u.bup"
-fixedplaylist="$HOME/.config/mpd/playlists/Favourites.m3u"
+playlist="$HOME/.config/mpd/playlists/Favourites.m3u"
 deletedplaylist="/tmp/deleted"
 if [ "$file" != "" ]; then
 	echo "$file" > "$deletedplaylist"
 	cp "$playlist" /tmp/cache
 	comm -13 <(sort "$deletedplaylist") <(sort "/tmp/cache") > "$playlist"
-	sort -u "$playlist" -o "$fixedplaylist"
+	sort -u "$playlist" -o "$playlist"
 	notify-send -t 3000 "MPD" "Successfully removed <b>${current//&/&amp;}</b> from Favourites"
 	exit 0
 else
